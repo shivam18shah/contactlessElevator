1e0ddc93507b68f0dd4f618b8e465e73ca0951ab
SarthakGadre <sugadre@asu.edu>
2020-10-30 17:16:44 -0500

    Function library and demo code for control

---
+++ finalCodes/controlLib.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 29 18:55:42 2020
+
+@author: pi
+"""
+
+# =============================================================================
+# Python lib for capturing commands and generating control signals
+# =============================================================================
+
+
+import re
+import time
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+class commandParser:
+    
+
+#Instead of separate floors as variables we can use lookup dict of premade commands    
+    def __init__(self):
+        self.trigSignal = 21
+        self.floor1Signal = 26
+        self.floor2Signal = 19
+        self.trigCommand = r"(okay|ok|hey)\s*elevator"
+        self.floor1Command = r"one|first|1"
+        self.floor2Command = r"second|two|2"
+        self.trigReceived = 0
+        GPIO.setup(self.trigSignal,GPIO.OUT)
+        GPIO.setup(self.floor1Signal,GPIO.OUT)
+        GPIO.setup(self.floor2Signal,GPIO.OUT)
+        
+    def firstFloor(self):
+        self.triggerIndicatorOFF()
+        print("Floor 1 control signal generated")
+        GPIO.output(self.floor1Signal,GPIO.HIGH)
+        time.sleep(5)
+        GPIO.output(self.floor1Signal,GPIO.LOW)
+        time.sleep(2)
+        
+    def secondFloor(self):
+        self.triggerIndicatorOFF()
+        print("Floor 2 control signal generated")
+        GPIO.output(self.floor2Signal,GPIO.HIGH)
+        time.sleep(5)
+        GPIO.output(self.floor2Signal,GPIO.LOW)
+        time.sleep(2)
+        
+    def triggerIndicatorON(self):
+        print("Trigger received, waiting for input")
+        GPIO.output(self.trigSignal,GPIO.HIGH)
+        
+    def triggerIndicatorOFF(self):
+        print("Thanks for input")
+        self.trigReceived=0
+        GPIO.output(self.trigSignal,GPIO.LOW)
+
+    def checkTrig(self,inp):
+        if re.search(self.trigCommand,inp,re.IGNORECASE):
+            self.trigReceived=1
+            self.triggerIndicatorON()
+        else:
+            print("Wrong Input")
+            self.triggerIndicatorOFF()
+            
+    def checkFloor(self,inp):
+        if re.search(self.floor1Command,inp,re.IGNORECASE):
+            self.firstFloor()
+        elif re.search(self.floor2Command,inp,re.IGNORECASE):
+            self.secondFloor()          
+        else:
+            print("Wrong Floor Input")
+            self.triggerIndicatorOFF()
+            
+            
---
+++ finalCodes/mainControl.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 29 21:42:09 2020
+
+@author: pi
+"""
+
+import controlLib
+import time
+from threading import Timer
+f = controlLib.commandParser()
+
+def input_with_timeout(x):
+    
+    def time_up():
+        print("Time up,try again")
+    
+    
+    t = Timer(x,time_up)
+    try:
+        t.start()
+        b = raw_input("Which floor?")
+    except Exception:
+        print("really")
+        b = None
+
+        
+    if b != True:
+        t.cancel()
+        
+input_with_timeout(3)
+#while True:
+#    a = raw_input("\nWhat is the command phrase?\n")
+#    f.checkTrig(a)
+#    recInp = 0
+#    if f.trigReceived:
+#        timeout = time.time() + 10.0
+#        while ((time.time() < timeout) or recInp == 0):
+#            b = raw_input("\nWhich floor?\n")
+#            recInp = 1
+#        if recInp:
+#            f.checkFloor(b)
+#        else:
+#            print("Say the floor within 10 sec")
+#            f.triggerIndicatorOFF()
+#    else:
+#        print("Incorrect Command try again\n")
