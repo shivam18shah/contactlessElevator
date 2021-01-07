f4825cfcd3ecb053c775e572fd41213123a07097
SarthakGadre <sugadre@asu.edu>
2021-01-07 11:55:23 -0600

    Code to test solid state relay functioning

---
+++ testCodes/ssdCheck.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  4 12:35:02 2021
+
+@author: pi
+"""
+
+import controlLib
+
+c = controlLib.controlCommands(19,21,20)  #Pin number according to the connections
+
+c.firstFloor()
