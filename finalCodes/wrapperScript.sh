d961287b9beb859557445af653e71b1e7e861b01
SarthakGadre <sugadre@asu.edu>
2021-01-21 11:55:31 -0600

    Wrapper to keep the python script running if it accidentally closes dure to expections

---
+++ finalCodes/wrapperScript.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+
+file="$PWD"/"$( basename "pidNo.txt" )"
+if [ ! -f "$file" ]
+then	
+	echo "Starting python script micStreaming.py"
+	python3 "$PWD"/"$( basename "micStreaming.py" )"
+else
+	curPID=$(<"$file")
+	PID="$(pgrep -f micStreaming.py)"
+	if [ "$curPID" != "$PID" ]
+	then
+		rm $file
+		python3 "$PWD"/"$( basename "micStreaming.py" )"	
+	else		
+		x=0
+	fi 
+fi
